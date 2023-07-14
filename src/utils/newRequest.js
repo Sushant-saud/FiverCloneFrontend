419b078d003cf2145558ec77df332d475d313f2a
unknown <Sushant-saud>
2023-07-14 06:42:44 -0700

    debug resole

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://api-sushantsaud9804-gmailcom.vercel.app/api/",
   withCredentials: true,
 });
 
